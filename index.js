16c93761f1d22c005dbce11db76cccab52e9cfe3
Teekay <tkmaile96@gmail.com>
2024-11-10 16:41:19 +0200

    Update Store class to use 'listeners' instead of 'listener', add subscribe and dispatch methods

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ class Store {
     constructor(reducer, initialState) {
         this.reducer =reducer;
         this.state = initialState;
-        this.listener = [];
+        this.listeners = [];
     }
 
     //return the current state
     getState() {
         console.log("Current State:", this.state);
         return this.state;
+    }
+    // Registers a new listener (observer)
+    subscribe(listener) {
+        this.listeners.push(listener);
+        // Unsubscribe function to remove listener
+        return () => {
+            this.listeners = this.listeners.filter(l => l !== listener);
+        };
+    }
+
+    // Dispatch an action and update state
+    dispatch(action) {
+        this.state = this.reducer(this.state, action);
+        this.listeners.forEach(listener => listener(this.state)); //Notify all listeners
+
     }
 }
